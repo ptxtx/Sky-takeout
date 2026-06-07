fdcfe2fe7f1041a7ca682dd5e861c519b955c14e
ptx <3351626369@qq.com>
2026-06-07 17:56:47 +0800

    营业额统计

---
+++ sky-server/src/main/java/com/sky/service/impl/ReportServiceImpl.java
@@ -0,0 +1,51 @@
+package com.sky.service.impl;
+
+import com.sky.entity.Orders;
+import com.sky.mapper.OrderMapper;
+import com.sky.service.ReportService;
+import com.sky.vo.TurnoverReportVO;
+import org.apache.commons.lang3.StringUtils;
+import org.apache.poi.util.StringUtil;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.LocalTime;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+@Service
+public class ReportServiceImpl implements ReportService {
+    @Autowired
+    private OrderMapper orderMapper;
+    public TurnoverReportVO getTurnoverStatistics(LocalDate begin, LocalDate end){
+        List<LocalDate> dateList=new ArrayList<>();//用于存放从begin到end范围内每一天的日期
+        dateList.add(begin);
+       while(!begin.equals(end)){
+           begin=begin.plusDays(1);
+           dateList.add(begin);
+       }
+       List<Double> turnoverList=new ArrayList<>();//存放每天的营业额
+       for(LocalDate date:dateList){
+           LocalDateTime beginTime = LocalDateTime.of(date, LocalTime.MIN);
+           LocalDateTime endTime = LocalDateTime.of(date, LocalTime.MAX);
+           Map map=new HashMap();
+           map.put("begin",beginTime);
+           map.put("end",endTime);
+           map.put("status", Orders.COMPLETED);
+           Double turnover=orderMapper.sumByMap(map);
+           turnover=turnover==null?0.0:turnover;
+           turnoverList.add(turnover);
+       }
+
+       return TurnoverReportVO.builder()
+               .dateList(StringUtils.join(dateList,","))
+               .turnoverList(StringUtils.join(turnoverList,","))
+               .build();
+
+    }
+
+}
